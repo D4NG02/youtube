8a6d1aa6ee3cda3ac160a124549388f260492b9b
Badrul Ijazi <badrulijazi@gmail.com>
2024-06-26 11:42:16 +0800

    [sibebar] add

--- src/Components/sideBar.tsx
+++ src/Components/sideBar.tsx
@@ -1,8 +1,15 @@
+import IconButton from "./IconButton";
+
+import IconHome from "@/Media/IconHome.svg";
+import IconShort from "@/Media/IconShort.svg";
+import IconSubscribetion from "@/Media/IconSubscribetion.svg";
 
 export default function SideBar() {
     return (
-        <div>
-            side
+        <div role="sibebar" className="h-full flex flex-row gap-2 justify-around sm:flex-col">
+            <IconButton className='bg-transparent w-full flex justify-center items-center sm:w-full sm:hover:cursor-pointer' src={IconHome} />
+            <IconButton className='bg-transparent w-full flex justify-center items-center sm:w-full sm:hover:cursor-pointer' src={IconShort} />
+            <IconButton className='bg-transparent w-full flex justify-center items-center sm:w-full sm:hover:cursor-pointer' src={IconSubscribetion} />
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="h-full">
       <Topbar />
-      <div className="px-4 py-2 h-[calc(100%-40px)] sm:h-[calc(100%-52px)] flex flex-col gap-2 sm:flex-row">
+      <div className="px-4 py-2 h-[calc(100%-40px)] sm:h-[calc(100%-52px)] flex flex-col gap-2 sm:p-2 sm:flex-row">
         <div className="order-2 h-[40px] sm:order-1 sm:w-[54px]">
           <SideBar />
         </div>
